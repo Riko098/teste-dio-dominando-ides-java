92abee79660c7bab09fff03109b6234bcd6e95e6
erico <ericopeixoto98@gmail.com>
2021-09-19 17:55:55 -0300

    first commit

---
+++ src/dio/com/br/Programa.java
@@ -0,0 +1,14 @@
+package dio.com.br;
+
+import dio.com.br.model.Gato;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Gato gato =  new Gato("Tom", "Marrom", 2);
+		System.out.println(gato);
+		System.out.println(gato.getNome());
+	}
+
+}
